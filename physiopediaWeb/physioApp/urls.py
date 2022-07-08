b4a30c6b3c14e7b871312a4199678c53350bc77e
Heidi Tan <heidi.tanyingxing@gmail.com>
2022-07-08 16:05:04 +0800

    test url

--- physiopediaWeb/physioApp/urls.py
+++ physiopediaWeb/physioApp/urls.py
@@ -2,5 +2,5 @@ from django.urls import include, path
 from .import views
 
 urlpatterns = [
-    path('')
+    path('currentMilestone/', views.displayMilestone, name='displayMilestone'),
 ]
--- physiopediaWeb/physioApp/views.py
+++ physiopediaWeb/physioApp/views.py
@@ -1,3 +1,12 @@
+from re import template
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template import loader
+
+def displayMilestone(request):
+    template = loader.get_template('currentMilestone.html')
+    return HttpResponse(template.render())
+

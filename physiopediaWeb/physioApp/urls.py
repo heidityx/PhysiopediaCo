3181100b3f8aa1c330d9c329433973be9f917aae
Heidi Tan <heidi.tanyingxing@gmail.com>
2022-07-12 16:44:51 +0800

    url links

--- physiopediaWeb/physioApp/urls.py
+++ physiopediaWeb/physioApp/urls.py
@@ -2,5 +2,16 @@ from django.urls import include, path
 from .import views
 
 urlpatterns = [
+    path('', views.displayStartup, name='displayStartup'),
+    path('login/', views.displayLogin, name='displayLogin'),
+    path('signUp/', views.displaySignUp, name='displaySignUp'),
+    path('selections/', views.displaySelection, name='displaySelection'),
+    path('selections-HeadNeckShoulder/', views.displaySelectHeadNeckShoulder, name='displaySelectHNS'),
+    path('selections-ChestBackWaist/', views.displaySelectChestBackWaist, name='displaySelectCBW'),
+    path('selections-ArmsHands/', views.displaySelectArmsHands, name='displaySelectAH'),
+    path('selections-Legs/', views.displaySelectLegs, name='displaySelectLegs'),
     path('currentMilestone/', views.displayMilestone, name='displayMilestone'),
+    path('log/', views.displayLog, name='displayLog'),
+    path('profile-settings/', views.displaySettings, name='displaySettings'),
+    path('store/', views.displayStore, name='displayStore'),
 ]
